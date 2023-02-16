cc44595a863dd6997efa8ded71741b213176c006
MustafaAdel <mustafaadelsoftwaredeveloper@gmail.com>
2023-02-16 16:21:33 +0200

    create a schema and model for tour document

--- server.js
+++ server.js
@@ -19,8 +19,25 @@ mongoose
   })
   .then(() => console.log('DB connection successful!'));
 
-const port = process.env.PORT || 3000;
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
 
+const Tour = mongoose.model('Tour', tourSchema);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`application running `);
 });
