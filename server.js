e1ac9e76b8e4856b4dcc7f6983cc6478bda2d1d0
MustafaAdel <mustafaadelsoftwaredeveloper@gmail.com>
2023-02-18 10:43:42 +0200

    refacting server.js from any additional code, make tour model

--- server.js
+++ server.js
@@ -19,35 +19,6 @@ mongoose
   })
   .then(() => console.log('DB connection successful!'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const tourInstance = new Tour({
-  name: 'The Park Camper',
-  price: 997,
-  rating: 4.5,
-});
-
-tourInstance
-  .save()
-  .then((doc) => console.log(doc))
-  .catch((err) => console.log(err));
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`application running `);
